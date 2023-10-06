690a095d861b65c2da4dee133ac5b9cc372f8cdf
12v7 <113438771+12v7@users.noreply.github.com>
2023-10-06 12:36:56 +0300

    Show examples of using at start

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # rclc-bot
 A telegram calculator bot.
 The bot uses the [teloxide](https://github.com/teloxide) bot framework and the [rclc](https://github.com/VladimirMarkelov/rclc_lib) mathematical library.
+Please have a look at [the rclc library](https://github.com/VladimirMarkelov/rclc_lib) to see all the functions available.
 
 If you would like to host your own bot, register your bot with [BotFather](https://t.me/botfather) and put the token in the [main.rs](https://github.com/12v7/rclc-bot/blob/main/src/main.rs) file.
 
 A ready-to-use bot is also available at [rclc_bot](https://t.me/rclc_bot).
+
+
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,12 @@ async fn main() {
 
             let mut cstate = parse::CalcState::new();
 
-            let text = match parse::eval(question, &mut cstate) {
-                Ok(val) => format!("{}", val),
-                Err(err) => format!("❌ {} ❌", err),
+            let text = match question {
+                "/start" => "Calculator bot. There are a few examples of its use:\n(1+2)(2+9)\n1\\2+3\\4\nsqrt(-4)\nfib(1000)\n0b100000000 + 0xFF".to_owned(),
+                _ => match parse::eval(question, &mut cstate) {
+                    Ok(val) => format!("{}", val),
+                    Err(err) => format!("❌ Error\n{}", err),
+                }
             };
 
             bot.send_message(msg.chat.id, text).await?;
